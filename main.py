daf71b39e7bf008011eede4353c95c6187f83665
Amith Reddy <amithmreddy@gmail.com>
2014-10-04 15:33:39 -0400

    Now you can edit any file you want with the url

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ class PostModel(ndb.Model):
 class BasicHandler(webapp2.RequestHandler):
     def render(self, template_name,**kwargs):
         template = JINJA_ESSAY.get_template(template_name)
-        self.response.out.write(template.render(**kwargs))   
-
+        self.response.out.write(template.render(**kwargs))
 class MainHandler(BasicHandler):
     def get(self):
         self.response.out.write('to be implemented')
@@ -45,34 +44,71 @@ class EssayList(BasicHandler):
                                     [essay.to_dict() for essay in essays]))
 
 class EditHandler(BasicHandler):
-    def get(self):
-        self.render("EssayEdit.html")
-    def post(self):
-        title =self.request.POST.get('title')
-        url = self.request.POST.get('url')
-        body = self.request.POST.get('body')
+    def get(self,*args):
+        if args[0] == 'newpost':
+            self.response.out.write("<h1>Newpost</h1>")
+            self.render("EssayEdit.html", keyid=None)
+        else:
+            q= ndb.gql("SELECT * FROM PostModel WHERE url=:url2",
+            url2=args[0])
+            
+            if q.get() !=None:
+                post= q.get().to_dict()
+                keyid= q.get().key.id()
+                
+                self.response.out.write("<h1>Editing %s</h1>"%post['title'])
+                self.render("EssayEdit.html",date=post['date'],
+                            url=post['url'],keyid=keyid,
+                            body=post['body'],title= post['title'])
+                            
+            else:
+                self.response.out.write("the essay %s doesn't exist" %args[0])
+    def post(self,*args):
+        keyid= self.request.get('keyid')
+        title =self.request.get('title')
+        url = self.request.get('url')
+        body = self.request.get('body')
         error= False
         if title and body and url:
-            condition = ndb.gql("SELECT * FROM PostModel WHERE url=:url2",
+            if args[0] == 'newpost':
+                post = ndb.gql("SELECT * FROM PostModel WHERE url=:url2",
                         url2=url).get()
-            if condition == None:
-                new_post = PostModel(title =title, body=body,url=url)
-                new_post.put()
-                time.sleep(1)
-                self.redirect('/essays/%s'%url)
+                if post['url'] == url:
+                    error="the url is already chosen please choose another one"
+                else:
+                    new_post = PostModel(title =title, body=body,url=url)
+                    new_post.put()
+            elif keyid != None:
+                update = PostModel.get_by_id(int(keyid))
+                dburl = ndb.gql("SELECT * FROM PostModel WHERE url=:url2",
+                        url2=url).get()
+                if dburl ==None: 
+                    update.title=title
+                    update.url=url
+                    update.body=body
+                    update.put()
+                elif dburl.url == update.url:
+                    update.title=title
+                    update.url=url
+                    update.body=body
+                    update.put()
+                else:
+                    error= "the url is already chosen please chose another one"
             else:
-                error="the url is already chosen please choose another one"
+                error= "something unexpected happend call the dev"
         else:
             error="all text must be filled"
 
         if error:
             self.render("EssayEdit.html",title=title,
-            body=body,url=url,error=error)
+            body=body,url=url,error=error,keyid=keyid)
+        else:
+            time.sleep(0.1)
+            self.redirect('/essays/%s'%url)
                    
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    #essay_id matches everything but a slash
     ('/essays/', EssayList),
     ('/essays/([^/]+)', BlogHandler),
-    ('/edit', EditHandler),
+    webapp2.Route(r'/essays/edit/<:[^?/]*>', handler=EditHandler),
 ], debug=True)
